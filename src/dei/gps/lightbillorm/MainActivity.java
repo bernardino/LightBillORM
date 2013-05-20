9b9099a5bff9973f0db3042d0511b3b301160422
bernardino <ricardo.bernardino@gmail.com>
2013-05-20 15:58:33 +0100

    new working .gitignore

--- src/dei/gps/lightbillorm/MainActivity.java
+++ src/dei/gps/lightbillorm/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends Activity {
 		//getApplicationContext().deleteDatabase("LightBill2.db");
 		
 		/* Vai buscar uma rotina, e se nao houver nenhuma cria */
+		
 		r = new Select().from(ModelRoutine.class).executeSingle();
 		if(r==null)
 			r = new ModelRoutine();
